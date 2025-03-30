9f71aa561b2923a080d5e3667881784ae3c98b28
gfixler <gary@garyfixler.com>
2025-03-30 08:31:54 -0500

    Replace Dubeolsik system with Unicode translation
    The previous system output English characters, which would then be
    converted to Korean letters and syllables through the computer's Korean
    keyboard layout. This required the user to switch over to the Korean
    layout first, before using this plugin.
    
    The new system uses some arithmetic, and numbers assigned to all the
    letters, to determine which of the ~11,000 Hangul syllable block code
    points to return. See the full table of Hangul syllables here:
    
    https://en.wikipedia.org/wiki/Hangul_Syllables
    
    Here's how the algorithm works...
    
    19 initial jamo:
    
    0. ᄀ 4. ㄸ  8. ㅃ 12. ㅈ 16. ㅌ
    1. ㄲ 5. ㄹ  9. ㅅ 13. ㅉ 17. ㅍ
    2. ㄴ 6. ㅁ 10. ㅆ 14. ㅊ 18. ㅎ
    3. ㄷ 7. ㅂ 11. ㅇ 15. ㅋ
    
    21 medial jamo:
    
    0. ㅏ  7. ㅖ 14. ㅝ
    1. ㅐ  8. ㅗ 15. ㅞ
    2. ㅑ  9. ㅘ 16. ㅟ
    3. ㅒ 10. ㅙ 17. ㅠ
    4. ㅓ 11. ㅚ 18. ㅡ
    5. ㅔ 12. ㅛ 19. ㅢ
    6. ㅕ 13. ㅜ 20. ㅣ
    
    27 final jamo (28 including "None"):
    
    0.     7. ㄷ 14. ㄿ 21. ㅇ
    1. ㄱ  8. ㄹ 15. ㅀ 22. ㅈ
    2. ㄲ  9. ㄺ 16. ㅁ 23. ㅊ
    3. ㄳ 10. ㄻ 17. ㅂ 24. ㅋ
    4. ㄴ 11. ㄼ 18. ㅄ 25. ㅌ
    5. ㄵ 12. ㄽ 19. ㅅ 26. ㅍ
    6. ㄶ 13. ㄾ 20. ㅆ 27. ㅎ
    
    We plug those numbers into this formula:
    
    44032 + initial * 588 + medial * 28 + final
    
    The first number gets us to the start of the Hangul set of characters in
    the Unicode system, and the rest offset us from there to the syllable
    that matches the parts. The 28 final characters repeat over and over,
    for each medial, and the medials repeat every 21 medials * 28 finals, or
    every 588 characters, and each of those repeats starts with a different
    initial character.
    
    Currently, I don't now how to slot in the "tense" final consonants—I
    just don't understand how those work in Hangul, so their contributions
    are currently set to 0—and I can't see how the compound final consonants
    are meant to be created. For the latter, I watched the demo video,
    explored the code, and did some looking around online, and never really
    saw them in use anywhere. When I understand these things, I can update
    the code.

--- easy_Korean_steno_chorded.py
+++ easy_Korean_steno_chorded.py
@@ -8,114 +8,114 @@ LONGEST_KEY = 1
 starting_consonants = {
     "regular": {
         # consonants
-        "K": "r",  # ㄱ
-        "TKPW": "r",  # ㄱ
-        "TPH": "s",  # ㄴ
-        "TK": "e",  # ㄷ
-        "R": "f",  # ㄹ
-        "HR": "f",  # ㄹ
-        "PH": "a",  # ㅁ
-        "PW": "q",  # ㅂ
-        "S": "t",  # ㅅ
-        "W": "d",  # ㅇ
-        "SKWR": "w",  # ㅈ
-        "KH": "c",  # ㅊ
-        "KP": "z",  # ㅋ
-        "T": "x",  # ㅌ
-        "P": "v",  # ㅍ
-        "H": "g",  # ㅎ
+        "K": 0,  # ㄱ
+        "TKPW": 0,  # ㄱ
+        "TPH": 2,  # ㄴ
+        "TK": 3,  # ㄷ
+        "R": 5,  # ㄹ
+        "HR": 5,  # ㄹ
+        "PH": 6,  # ㅁ
+        "PW": 7,  # ㅂ
+        "S": 9,  # ㅅ
+        "W": 11,  # ㅇ
+        "SKWR": 12,  # ㅈ
+        "KH": 14,  # ㅊ
+        "KP": 15,  # ㅋ
+        "T": 16,  # ㅌ
+        "P": 17,  # ㅍ
+        "H": 18,  # ㅎ
         # extra
-        "TH": "E",  # ㄸ
-        "": "d",  # ㅇ
+        "TH": 4,  # ㄸ
+        "": 11,  # ㅇ
     },
     "tense": {
         # tense consonants (add *)
-        "K": "R",  # ㄲ
-        "TKPW": "R",  # ㄲ
-        "TK": "E",  # ㄸ
-        "PW": "Q",  # ㅃ
-        "SKWR": "W",  # ㅉ
-        "S": "T",  # ㅆ
+        "K": 1,  # ㄲ
+        "TKPW": 1,  # ㄲ
+        "TK": 4,  # ㄸ
+        "PW": 8,  # ㅃ
+        "SKWR": 13,  # ㅉ
+        "S": 10,  # ㅆ
     },
     "special": {
         # adds "y" to vowels
-        "KW": "r",  # ㄱ
-        "TKPWR": "r",  # ㄱ
-        "TPWH": "s",  # ㄴ
-        "TKW": "e",  # ㄷ
-        "WR": "f",  # ㄹ
-        "WHR": "f",  # ㄹ
-        "KPWHR": "a",  # ㅁ
-        "KPWR": "q",  # ㅂ
-        "SH": "t",  # ㅅ
-        "KWR": "d",  # ㅇ
-        "SKW": "w",  # ㅈ
-        "KWH": "c",  # ㅊ
-        "KPW": "z",  # ㅋ
-        "TKWR": "x",  # ㅌ
-        "KPR": "v",  # ㅍ
-        "WH": "g",  # ㅎ
-        "STKPWHR": "",  # empty
+        "KW": 0,  # ㄱ
+        "TKPWR": 0,  # ㄱ
+        "TPWH": 2,  # ㄴ
+        "TKW": 3,  # ㄷ
+        "WR": 5,  # ㄹ
+        "WHR": 5,  # ㄹ
+        "KPWHR": 6,  # ㅁ
+        "KPWR": 7,  # ㅂ
+        "SH": 9,  # ㅅ
+        "KWR": 11,  # ㅇ
+        "SKW": 12,  # ㅈ
+        "KWH": 14,  # ㅊ
+        "KPW": 15,  # ㅋ
+        "TKWR": 16,  # ㅌ
+        "KPR": 17,  # ㅍ
+        "WH": 18,  # ㅎ
+        "STKPWHR": 0,  # empty
     },
 }
 vowels = {
     # vowels
-    "A": "k",  # ㅏ
-    "U": "j",  # ㅓ
-    "O": "h",  # ㅗ
-    "AO": "n",  # ㅜ
-    "AOU": "m",  # ㅡ
-    "AU": "m",  # ㅡ
-    "EU": "l",  # ㅣ
+    "A": 0,  # ㅏ
+    "U": 4,  # ㅓ
+    "O": 8,  # ㅗ
+    "AO": 13,  # ㅜ
+    "AOU": 18,  # ㅡ
+    "AU": 18,  # ㅡ
+    "EU": 20,  # ㅣ
     # complex vowels
-    "AEU": "o",  # ㅐ
-    "AE": "p",  # ㅔ
+    "AEU": 1,  # ㅐ
+    "AE": 5,  # ㅔ
     # extra/compound
-    "E": "ml",  # ㅢ
-    "OEU": "hl",  # ㅚ
-    "AOU": "nj",  # ㅝ
-    "OU": "hk",  # ㅘ
-    "AOEU": "nl",  # ㅟ
-    "": "",  # empty
+    "E": 19,  # ㅢ
+    "OEU": 11,  # ㅚ
+    "AOU": 14,  # ㅝ
+    "OU": 9,  # ㅘ
+    "AOEU": 16,  # ㅟ
+    "": 0,  # empty
 }
 y_vowels = {
     # "y" vowels
-    "A": "i",  # ㅑ
-    "U": "u",  # ㅕ
-    "O": "y",  # ㅛ
-    "OE": "y",  # ㅛ
-    "AOU": "b",  # ㅠ
-    "AO": "b",  # ㅠ
-    "AEU": "O",  # ㅒ
-    "AE": "P",  # ㅖ
-    "EU": "l",  # ㅣ
-    "": "",  # empty
+    "A": 2,  # ㅑ
+    "U": 6,  # ㅕ
+    "O": 12,  # ㅛ
+    "OE": 12,  # ㅛ
+    "AOU": 17,  # ㅠ
+    "AO": 17,  # ㅠ
+    "AEU": 3,  # ㅒ
+    "AE": 7,  # ㅖ
+    "EU": 20,  # ㅣ
+    "": 0,  # empty
 }
 ending_consonants = {
     # consonants
-    "G": "r",  # ㄱ
-    "PB": "s",  # ㄴ
-    "D": "e",  # ㄷ
-    "R": "f",  # ㄹ
-    "L": "f",  # ㄹ
-    "PL": "a",  # ㅁ
-    "B": "q",  # ㅂ
-    "S": "t",  # ㅅ
-    "PBG": "d",  # ㅇ
-    "PBLG": "w",  # ㅈ
-    "FP": "c",  # ㅊ
-    "BG": "z",  # ㅋ
-    "T": "x",  # ㅌ
-    "P": "v",  # ㅍ
-    "F": "g",  # ㅎ
+    "G": 1,  # ㄱ
+    "PB": 4,  # ㄴ
+    "D": 7,  # ㄷ
+    "R": 8,  # ㄹ
+    "L": 8,  # ㄹ
+    "PL": 16,  # ㅁ
+    "B": 17,  # ㅂ
+    "S": 19,  # ㅅ
+    "PBG": 21,  # ㅇ
+    "PBLG": 22,  # ㅈ
+    "FP": 23,  # ㅊ
+    "BG": 24,  # ㅋ
+    "T": 25,  # ㅌ
+    "P": 26,  # ㅍ
+    "F": 27,  # ㅎ
     # tense consonants (add -R)
-    "RG": "R",  # ㄲ
-    "RD": "E",  # ㄸ
-    "RB": "Q",  # ㅃ
-    "RPBLG": "W",  # ㅉ
-    "RS": "T",  # ㅆ
-    "Z": "T",  # ㅆ
-    "": "",  # empty
+    "RG": 0,  # ㄲ
+    "RD": 0,  # ㄸ
+    "RB": 0,  # ㅃ
+    "RPBLG": 0,  # ㅉ
+    "RS": 0,  # ㅆ
+    "Z": 0,  # ㅆ
+    "": 0,  # empty
 }
 
 
@@ -172,18 +172,21 @@ def lookup(chord):
         vowel_final = vowels[vowel]
 
     # only vowel output using *
-    if stress == "*" and start_consonant == "":
-        start_final = ""
+    if stress == "*" and start_consonant == 0:
+        start_final = 0
 
     # get end consonant
     if end_consonant not in ending_consonants:
         raise KeyError
     # only end output
-    if end_consonant and start_consonant == "":
-        start_final = ""
+    if end_consonant == 0 and start_consonant == 0:
+        start_final = 0
+    end_final = ending_consonants[end_consonant]
 
     # combine output
-    output = (
-        "{^}" + start_final + vowel_final + ending_consonants[end_consonant] + "{^}"
-    )
-    return output
+    hangul_offset = 44032
+    initial = start_final * 588
+    medial = vowel_final * 28
+    final = end_final
+    code_point = hangul_offset + initial + medial + final
+    return "{^}" + chr(code_point) + "{^}"
