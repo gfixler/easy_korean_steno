92892ac1f3e573d3d7dcffe7dced23867f49302e
gfixler <gary@garyfixler.com>
2025-03-31 02:21:57 -0500

    Pair each jamo offset with its jamo
    This moves the jamo comments after each dictionary entry into a pair
    with their offset integers, reifying them as data we can use. This is in
    preparation for being able to output each on its own.

--- easy_Korean_steno_chorded.py
+++ easy_Korean_steno_chorded.py
@@ -5,121 +5,121 @@ LONGEST_KEY = 1
 # list of keys for consonants and vowels
 starting_consonants = {
     "regular": {
-        "K": 0,  # ㄱ
-        "TKPW": 0,  # ㄱ
-        "TPH": 2,  # ㄴ
-        "TK": 3,  # ㄷ
-        "TH": 4,  # ㄸ
-        "R": 5,  # ㄹ
-        "HR": 5,  # ㄹ
-        "PH": 6,  # ㅁ
-        "PW": 7,  # ㅂ
-        "S": 9,  # ㅅ
-        "": 11,  # ㅇ
-        "W": 11,  # ㅇ
-        "SKWR": 12,  # ㅈ
-        "KH": 14,  # ㅊ
-        "KP": 15,  # ㅋ
-        "T": 16,  # ㅌ
-        "P": 17,  # ㅍ
-        "H": 18,  # ㅎ
+        "K": (0, "ㄱ"),
+        "TKPW": (0, "ㄱ"),
+        "TPH": (2, "ㄴ"),
+        "TK": (3, "ㄷ"),
+        "TH": (4, "ㄸ"),
+        "R": (5, "ㄹ"),
+        "HR": (5, "ㄹ"),
+        "PH": (6, "ㅁ"),
+        "PW": (7, "ㅂ"),
+        "S": (9, "ㅅ"),
+        "": (11, "ㅇ"),
+        "W": (11, "ㅇ"),
+        "SKWR": (12, "ㅈ"),
+        "KH": (14, "ㅊ"),
+        "KP": (15, "ㅋ"),
+        "T": (16, "ㅌ"),
+        "P": (17, "ㅍ"),
+        "H": (18, "ㅎ"),
     },
     "tense": {
         # tense consonants (add *)
-        "K": 1,  # ㄲ
-        "TKPW": 1,  # ㄲ
-        "TK": 4,  # ㄸ
-        "PW": 8,  # ㅃ
-        "S": 10,  # ㅆ
-        "SKWR": 13,  # ㅉ
+        "K": (1, "ㄲ"),
+        "TKPW": (1, "ㄲ"),
+        "TK": (4, "ㄸ"),
+        "PW": (8, "ㅃ"),
+        "S": (10, "ㅆ"),
+        "SKWR": (13, "ㅉ"),
     },
     "special": {
         # adds "y" to vowels
-        "KW": 0,  # ㄱ
-        "TKPWR": 0,  # ㄱ
-        "TPWH": 2,  # ㄴ
-        "TKW": 3,  # ㄷ
-        "WR": 5,  # ㄹ
-        "WHR": 5,  # ㄹ
-        "KPWHR": 6,  # ㅁ
-        "KPWR": 7,  # ㅂ
-        "SH": 9,  # ㅅ
-        "KWR": 11,  # ㅇ
-        "SKW": 12,  # ㅈ
-        "KWH": 14,  # ㅊ
-        "KPW": 15,  # ㅋ
-        "TKWR": 16,  # ㅌ
-        "KPR": 17,  # ㅍ
-        "WH": 18,  # ㅎ
-        "STKPWHR": 0,  # empty
+        "KW": (0, "ㄱ"),
+        "TKPWR": (0, "ㄱ"),
+        "TPWH": (2, "ㄴ"),
+        "TKW": (3, "ㄷ"),
+        "WR": (5, "ㄹ"),
+        "WHR": (5, "ㄹ"),
+        "KPWHR": (6, "ㅁ"),
+        "KPWR": (7, "ㅂ"),
+        "SH": (9, "ㅅ"),
+        "KWR": (11, "ㅇ"),
+        "SKW": (12, "ㅈ"),
+        "KWH": (14, "ㅊ"),
+        "KPW": (15, "ㅋ"),
+        "TKWR": (16, "ㅌ"),
+        "KPR": (17, "ㅍ"),
+        "WH": (18, "ㅎ"),
+        "STKPWHR": (0, ""), # empty
     },
 }
 vowels = {
-    "": 0,  # empty
-    "A": 0,  # ㅏ
-    "AEU": 1,  # ㅐ
-    "U": 4,  # ㅓ
-    "AE": 5,  # ㅔ
-    "O": 8,  # ㅗ
-    "OU": 9,  # ㅘ
-    "OEU": 11,  # ㅚ
-    "AO": 13,  # ㅜ
-    "AOU": 14,  # ㅝ
-    "AOEU": 16,  # ㅟ
-    "AOU": 18,  # ㅡ
-    "AU": 18,  # ㅡ
-    "E": 19,  # ㅢ
-    "EU": 20,  # ㅣ
+    "": (0, ""), # empty
+    "A": (0, "ㅏ"),
+    "AEU": (1, "ㅐ"),
+    "U": (4, "ㅓ"),
+    "AE": (5, "ㅔ"),
+    "O": (8, "ㅗ"),
+    "OU": (9, "ㅘ"),
+    "OEU": (11, "ㅚ"),
+    "AO": (13, "ㅜ"),
+    "AOU": (14, "ㅝ"),
+    "AOEU": (16, "ㅟ"),
+    "AOU": (18, "ㅡ"),
+    "AU": (18, "ㅡ"),
+    "E": (19, "ㅢ"),
+    "EU": (20, "ㅣ"),
 }
 y_vowels = {
-    "": 0,  # empty
-    "A": 2,  # ㅑ
-    "AEU": 3,  # ㅒ
-    "U": 6,  # ㅕ
-    "AE": 7,  # ㅖ
-    "O": 12,  # ㅛ
-    "OE": 12,  # ㅛ
-    "AOU": 17,  # ㅠ
-    "AO": 17,  # ㅠ
-    "EU": 20,  # ㅣ
+    "": (0, ""), # empty
+    "A": (2, "ㅑ"),
+    "AEU": (3, "ㅒ"),
+    "U": (6, "ㅕ"),
+    "AE": (7, "ㅖ"),
+    "O": (12, "ㅛ"),
+    "OE": (12, "ㅛ"),
+    "AOU": (17, "ㅠ"),
+    "AO": (17, "ㅠ"),
+    "EU": (20, "ㅣ"),
 }
 ending_consonants = {
-    "": 0,  # empty
-    "G": 1,  # ㄱ
-    "GT": 2,  # ㄲ
-    "GZ": 2,  # ㄲ
-    "GS": 3,  # ㄳ
-    "PB": 4,  # ㄴ
-    "PBG": 5,  # ㄵ
-    "FPB": 6,  # ㄶ
-    "D": 7,  # ㄷ
-    "R": 8,  # ㄹ
-    "L": 8,  # ㄹ
-    "RG": 9,  # ㄺ
-    "LG": 9,  # ㄺ
-    "RPL": 10,  # ㄻ
-    "RB": 11,  # ㄼ
-    "RS": 12,  # ㄽ
-    "LS": 12,  # ㄽ
-    "RT": 13,  # ㄾ
-    "LT": 13,  # ㄾ
-    "RP": 14,  # ㄿ
-    "FR": 15,  # ㅀ
-    "FL": 15,  # ㅀ
-    "PL": 16,  # ㅁ
-    "B": 17,  # ㅂ
-    "BS": 18,  # ㅄ
-    "BZ": 18,  # ㅄ
-    "S": 19,  # ㅅ
-    "SZ": 20,  # ㅆ
-    "TS": 20,  # ㅆ
-    "PBG": 21,  # ㅇ
-    "PBLG": 22,  # ㅈ
-    "FP": 23,  # ㅊ
-    "BG": 24,  # ㅋ
-    "T": 25,  # ㅌ
-    "P": 26,  # ㅍ
-    "F": 27,  # ㅎ
+    "": (0, ""), # empty
+    "G": (1, "ㄱ"),
+    "GT": (2, "ㄲ"),
+    "GZ": (2, "ㄲ"),
+    "GS": (3, "ㄳ"),
+    "PB": (4, "ㄴ"),
+    "PBG": (5, "ㄵ"),
+    "FPB": (6, "ㄶ"),
+    "D": (7, "ㄷ"),
+    "R": (8, "ㄹ"),
+    "L": (8, "ㄹ"),
+    "RG": (9, "ㄺ"),
+    "LG": (9, "ㄺ"),
+    "RPL": (10, "ㄻ"),
+    "RB": (11, "ㄼ"),
+    "RS": (12, "ㄽ"),
+    "LS": (12, "ㄽ"),
+    "RT": (13, "ㄾ"),
+    "LT": (13, "ㄾ"),
+    "RP": (14, "ㄿ"),
+    "FR": (15, "ㅀ"),
+    "FL": (15, "ㅀ"),
+    "PL": (16, "ㅁ"),
+    "B": (17, "ㅂ"),
+    "BS": (18, "ㅄ"),
+    "BZ": (18, "ㅄ"),
+    "S": (19, "ㅅ"),
+    "SZ": (20, "ㅆ"),
+    "TS": (20, "ㅆ"),
+    "PBG": (21, "ㅇ"),
+    "PBLG": (22, "ㅈ"),
+    "FP": (23, "ㅊ"),
+    "BG": (24, "ㅋ"),
+    "T": (25, "ㅌ"),
+    "P": (26, "ㅍ"),
+    "F": (27, "ㅎ"),
 }
 
 
@@ -156,12 +156,12 @@ def lookup(chord):
     add_y = False
     # detect stress
     if stress == "*" and start_consonant in starting_consonants["tense"]:
-        start_final = starting_consonants["tense"][start_consonant]
+        start_offset, start_final = starting_consonants["tense"][start_consonant]
     elif start_consonant in starting_consonants["regular"]:
-        start_final = starting_consonants["regular"][start_consonant]
+        start_offset, start_final = starting_consonants["regular"][start_consonant]
     elif start_consonant in starting_consonants["special"]:
         add_y = True
-        start_final = starting_consonants["special"][start_consonant]
+        start_offset, start_final = starting_consonants["special"][start_consonant]
     else:
         raise KeyError
 
@@ -172,9 +172,9 @@ def lookup(chord):
     if start_consonant in starting_consonants["special"] and not vowel:
         raise KeyError
     if add_y:
-        vowel_final = y_vowels[vowel]
+        vowel_offset, vowel_final = y_vowels[vowel]
     else:
-        vowel_final = vowels[vowel]
+        vowel_offset, vowel_final = vowels[vowel]
 
     # only vowel output using *
     if stress == "*" and start_consonant == 0:
@@ -186,12 +186,12 @@ def lookup(chord):
     # only end output
     if end_consonant == 0 and start_consonant == 0:
         start_final = 0
-    end_final = ending_consonants[end_consonant]
+    end_offset, end_final = ending_consonants[end_consonant]
 
     # combine output
     hangul_offset = 44032
-    initial = start_final * 588
-    medial = vowel_final * 28
-    final = end_final
+    initial = start_offset * 588
+    medial = vowel_offset * 28
+    final = end_offset
     code_point = hangul_offset + initial + medial + final
     return "{^}" + chr(code_point) + "{^}"
